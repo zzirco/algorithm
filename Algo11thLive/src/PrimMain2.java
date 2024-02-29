d9a8514f37253aa23a27989c9b7622d287117af3
zzirco <zzirco@naver.com>
2024-02-29 16:31:07 +0900

    0229

--- Algo11thLive/src/PrimMain2.java
+++ Algo11thLive/src/PrimMain2.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class PrimMain2 {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int[][] g = new int[N][N];
@@ -15,8 +15,7 @@ public class PrimMain2 {
 			}
 			minEdge[i] = Integer.MAX_VALUE;
 		}
-		
-		int result=0, cnt=0;
+		int result = 0, cnt = 0;
 		minEdge[0] = 0;
 		for(int i=0; i<N; i++) {
 			int minVertex = -1;
--- baekjoon/src/baekjoon/Solution_bj_2206_벽부수고이동하기.java
+++ baekjoon/src/baekjoon/Solution_bj_2206_벽부수고이동하기.java
@@ -4,44 +4,42 @@ import java.io.*;
 import java.util.*;
 
 public class Solution_bj_2206_벽부수고이동하기 {
-	static final int[] di = {0,0,-1,1};
-	static final int[] dj = {-1,1,0,0};
+	static final int[] di = {-1,1,0,0};
+	static final int[] dj = {0,0,-1,1};
 	static int N, M;
 	static int[][] arr;
-	static boolean[][] v;
+	static boolean[][][] v;
 	static int ans;
-	static int wallCnt;
-	static int bfs(int i, int j, int cnt) {
+	static int bfs(int i, int j, int cnt, int wallCnt) {
 		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] {i,j,cnt});
-		v[i][j] = true;
+		q.offer(new int[] {i,j,cnt,wallCnt});
+		v[i][j][wallCnt] = true;
 		while(!q.isEmpty()) {
 			int[] ij = q.poll();
 			i = ij[0];
 			j = ij[1];
 			cnt = ij[2];
+			wallCnt = ij[3];
+			if(i==N-1&&j==M-1) {
+				ans = cnt;
+				return ans;
+			}
 			for(int d=0; d<4; d++) {
 				int ni = i + di[d];
 				int nj = j + dj[d];
-				if(ni>=0&&ni<N&&nj>=0&&nj<M&&!v[ni][nj]) {
-					if(ni==N-1&&nj==M-1) {
-						ans = cnt + 1;
-						return ans;
+				if(ni>=0&&ni<N&&nj>=0&&nj<M) {
+					if(arr[ni][nj]==1&&!v[ni][nj][wallCnt]&&wallCnt==0) {
+						v[ni][nj][1] = true;
+						q.offer(new int[] {ni,nj,cnt+1,wallCnt+1});
 					}
-					if(arr[ni][nj]==1) {
-						if(wallCnt==1) {
-							wallCnt--;
-							arr[ni][nj] = 0;
-						} else {
-							continue;
-						}
+					if(arr[ni][nj]==0&&!v[ni][nj][wallCnt]) {
+						v[ni][nj][wallCnt] = true;
+						q.offer(new int[] {ni,nj,cnt+1,wallCnt});
 					}
-					v[ni][nj] = true;
-					q.offer(new int[] {ni,nj,cnt+1});
 				}
 			}
 		}
-		return ans;
+		return -1;
 	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -49,15 +47,13 @@ public class Solution_bj_2206_벽부수고이동하기 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new int[N][M];
-		v = new boolean[N][M];
-		ans = -1;
-		wallCnt = 1;
+		v = new boolean[N][M][2];
 		for(int i=0; i<N; i++) {
 			String[] sa = br.readLine().split("");
 			for(int j=0; j<M; j++) {
 				arr[i][j] = Integer.parseInt(sa[j]);
 			}
 		}
-		System.out.println(bfs(0,0,1));
+		System.out.println(bfs(0,0,1,0));
 	}
 }
