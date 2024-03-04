abf1ff6d00b096eb0da1b5a53da63fd5a7abb741
zzirco <zzirco@naver.com>
2024-03-04 21:57:01 +0900

    0304

--- baekjoon/src/baekjoon/Solution_bj_15663_N과M9.java
+++ baekjoon/src/baekjoon/Solution_bj_15663_N과M9.java
@@ -5,21 +5,25 @@ import java.util.*;
 
 public class Solution_bj_15663_N과M9 {
 	static int N, M;
-	static HashSet<Integer> arr;
-	static TreeSet<Integer> a;
-	static int[] b;
+	static int[] a, b;
+	static LinkedHashSet<Integer> lhs;
 	static StringBuilder sb = new StringBuilder();
 	static void comb(int cnt, int start) {
 		if(cnt==M) {
+			lhs = new LinkedHashSet<>();
 			for(int i:b) {
+				lhs.add(i);
+			}
+			System.out.println("lhs:"+lhs);
+			for(int i:lhs) {
 				sb.append(i+" ");
 			}
 			sb.append("\n");
 			return;
 		}
 		for(int i=start; i<N; i++) {
-			b[cnt] = a.;
-			comb(cnt+1,i);
+			b[cnt] = a[i];
+			comb(cnt+1,i+1);
 		}
 	}
 	public static void main(String[] args) throws Exception {
@@ -27,14 +31,13 @@ public class Solution_bj_15663_N과M9 {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		arr = new HashSet<>();
+		a = new int[N];
 		b = new int[M];
+		lhs = new LinkedHashSet<>();
 		st = new StringTokenizer(br.readLine(), " ");
 		for(int i=0; i<N; i++) {
-			arr.add(Integer.parseInt(st.nextToken()));
+			a[i] = Integer.parseInt(st.nextToken());
 		}
-		TreeSet<Integer> a = new TreeSet<>();
-		a.addAll(arr);
 		comb(0,0);
 		System.out.println(sb);
 	}
