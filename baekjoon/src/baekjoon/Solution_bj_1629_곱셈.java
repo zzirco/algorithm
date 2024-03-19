d2431cb80b65a12496d5d55f403c03063f401e61
zzirco <zzirco@naver.com>
2024-03-19 17:49:45 +0900

    0319

---
+++ baekjoon/src/baekjoon/Solution_bj_1629_곱셈.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution_bj_1629_곱셈 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int AmodC = A%C;
+		int BmodC = B%C;
+		
+	}
+}
