b61015629a55fc8b4406120d89fe8166138aaa6b
zzirco <zzirco@naver.com>
2024-03-27 17:55:28 +0900

    0327

---
+++ baekjoon/src/baekjoon/Solution_bj_2089_외판원순회.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution_bj_2089_외판원순회 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	}
+}
