a12a6430c4870917594d19e97d67c64dd7b39782
zzirco <zzirco@naver.com>
2024-04-20 22:27:16 +0900

    0420

---
+++ Pro/src/No2/새로운불면증치료법.java
@@ -0,0 +1,5 @@
+package No2;
+
+public class 새로운불면증치료법 {
+
+}
---
+++ Pro/src/새로운불면증치료법.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class 새로운불면증치료법 {
+	public static void main(String[] args) throws Exception {
+		
+	}
+}
