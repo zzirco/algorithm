d3eea3ee0fd6a9efcac85e7ce1f12d389ce31aed
zzirco <zzirco@naver.com>
2024-04-25 21:56:44 +0900

    0425

---
+++ baekjoon/src/baekjoon/Solution_bj_1000_AB.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.*;
+
+public class Solution_bj_1000_AB {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double A = sc.nextDouble();
+		double B = sc.nextDouble();
+		System.out.println(A/B);
+		sc.close();
+	}
+}
