d3f9c4c3a79cca31be112e64158c6330c2905739
zzirco <zzirco@naver.com>
2024-07-24 08:37:04 +0900

    0724

---
+++ baekjoon/src/baekjoon/Solution_bj_10869_사칙연산.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.*;
+
+public class Solution_bj_10869_사칙연산 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+		sc.close();
+	}
+}
