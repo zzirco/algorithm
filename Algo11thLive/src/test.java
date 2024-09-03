d75e069563998178dadc19c11057aa430deec10e
zzirco <zzirco@naver.com>
2024-09-03 15:52:46 +0900

    0903

--- Algo11thLive/src/test.java
+++ Algo11thLive/src/test.java
@@ -2,28 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class test {
-    static int N;
-    static int[] arr;
-    static int[] cmp = new int[2];
-    static int max = Integer.MIN_VALUE;
-    static void comb(int cnt, int start) {
-        if(cnt==2) {
-            int mul = cmp[0] * cmp[1];
-            max = Math.max(max, mul);
-            return;
-        }
-        for(int i=start; i<N; i++) {
-            cmp[cnt] = arr[i];
-            comb(cnt+1, i+1);
-        }
-    }
+	static class Node {
+		int x;
+		int y;
+		public int getX() {
+			return x;
+		}
+		public void setX(int x) {
+			this.x = x;
+		}
+		public int getY() {
+			return y;
+		}
+		public void setY(int y) {
+			this.y = y;
+		}
+		public Node(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+		@Override
+		public String toString() {
+			return "Node [x=" + x + ", y=" + y + "]";
+		}
+		
+	}
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        for(int i=0; i<N; i++) {
-            arr[i] = sc.nextInt();
-        }
-        comb(0,0);
-        System.out.println(max);
+    	int x = 0, y = 0;
+    	int index = 5;
+    	Node[] n = new Node[index];
+    	for(int i=0; i<index; i++) {
+    		n[i] = new Node(i,i);
+    	}
+		System.out.println(Arrays.toString(n));
     }
 }
