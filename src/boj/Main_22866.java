6bca5ded59b73b35ff10fc04125041434aa5d107
zzirco <zzirco@naver.com>
2025-07-24 17:45:10 +0900

    [feat] BOJ 22866 탑보기 입출력 작성

---
+++ src/boj/Main_22866.java
@@ -0,0 +1,17 @@
+package boj;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_22866 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
